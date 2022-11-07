772a73f9623edd5196198f4c4ec3ff0a817c7d6a
Justyna Komorowska <justyna.komorowska@lingarogroup.com>
2022-11-07 21:53:18 +0100

    play added

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # table_ordering
 Speech recognition app for table ordering use case.
+For Polish speakers.
 
 # references
 
@@ -10,9 +11,18 @@ Speech recognition app for table ordering use case.
 
 All commands below to be rum in terminal
 
+```bash
 pip install https://github.com/huggingface/transformers/archive/refs/heads/master.zip
 pip install https://github.com/kensho-technologies/pyctcdecode/archive/refs/heads/main.zip
-pip install https://github.com/kpu/kenlm/archive/master.zip
+pip install https://github.com/kpu/kenlm/archive/master.zip # ignore
 pip install wavio
 pip install jiwer
 pip install arpa
+
+```
+### **record.py** records what you're saying.
+### **sr.py** stands for speech recognition. Blackbox model downloaded from HF transcripts the speech.
+
+Examples:
+1. run examples_pyaudio/record.py - output.wav will be generated 
+2. run examples_zum2sr.py - recorded text will be recognized. Best attempt.
--- examples_pyaudio/record.py
+++ examples_pyaudio/record.py
@@ -10,7 +10,7 @@ CHANNELS = 1
 RATE = 16000
 RECORD_SECONDS = 5
 WAVE_OUTPUT_FILENAME = "output.wav"
-
+"""
 p = pyaudio.PyAudio()
 
 stream = p.open(format=FORMAT,
@@ -39,3 +39,39 @@ wf.setsampwidth(p.get_sample_size(FORMAT))
 wf.setframerate(RATE)
 wf.writeframes(b''.join(frames))
 wf.close()
+"""
+def record(fileout=WAVE_OUTPUT_FILENAME, seconds=RECORD_SECONDS):
+    p = pyaudio.PyAudio()
+
+    stream = p.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    frames_per_buffer=CHUNK)
+
+    print("* recording")
+
+    frames = []
+
+    for i in range(0, int(RATE / CHUNK * seconds)):
+        data = stream.read(CHUNK)
+        frames.append(data)
+
+    print("* done recording")
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+    wf = wave.open(fileout, 'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(p.get_sample_size(FORMAT))
+    wf.setframerate(RATE)
+    wf.writeframes(b''.join(frames))
+    wf.close()
+
+if __name__ == "__main__":
+    fil = "nagranie1.wav"
+    record(fil)
+    fil = "nagranie2.wav"
+    record(fil)
